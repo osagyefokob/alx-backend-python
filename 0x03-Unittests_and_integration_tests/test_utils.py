e6aa1ba6f33fdc34c3c129b7cc96e9666ebad031
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-15 06:58:57 +0000

    Create unittests for access_nested_map function
    Added unittests for the access_nested_map function in utils.

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Unittests for utils.access_nested_map - Task 0"""
+
+import unittest
+from parameterized import parameterized
+import utils
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
