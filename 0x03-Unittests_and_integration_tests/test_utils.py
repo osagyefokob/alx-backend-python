51ed10306820e130fcb28a68ae97ba553adf74d3
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-16 03:59:18 +0000

    Update unit tests for access_nested_map function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Unit tests for utils module"""
+"""Unit tests for utils.access_nested_map"""
 
 import unittest
 from parameterized import parameterized
@@ -10,11 +10,20 @@ class TestAccessNestedMap(unittest.TestCase):
     """Tests for access_nested_map"""
 
     @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test access_nested_map returns the expected output."""
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """Test KeyError is raised with correct message"""
+        """Test KeyError is raised with correct message."""
         with self.assertRaises(KeyError) as context:
             utils.access_nested_map(nested_map, path)
         expected = f"'{path[-1]}'"
