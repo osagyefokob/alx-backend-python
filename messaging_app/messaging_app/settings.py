e96ed5990d713aa8979d1e864c99e746649d9616
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-18 10:03:06 +0000

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -1,19 +1,22 @@
-REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
-}
-
-
-INSTALLED_APPS = [
-    "rest_framework",
-    "rest_framework_simplejwt",
-]
+# messaging_app/messaging_app/settings.py
+# add or update this REST_FRAMEWORK block
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    # default pagination class for the project
+    "DEFAULT_PAGINATION_CLASS": "chats.pagination.StandardResultsSetPagination",
+    "PAGE_SIZE": 20,
+    # filtering backends (django-filter is required)
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
+    ],
 }
