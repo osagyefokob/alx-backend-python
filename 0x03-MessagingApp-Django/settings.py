2701dcdbfe99e8f1ce258de02046f33f1dbf2157
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-18 11:17:04 +0000

    Delete 0x03-MessagingApp-Django directory

--- 0x03-MessagingApp-Django/settings.py
+++
@@ -1,6 +0,0 @@
-# ALX checker placeholder settings.py
-# Checker only needs this file to exist and contain the middleware line.
-
-MIDDLEWARE = [
-    "chats.middleware.RequestLoggingMiddleware",
-]
