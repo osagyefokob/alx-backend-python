4190239b14ae2de21e29c7deae40c49f1fc1c051
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-18 10:41:29 +0000

    Create urls.py

---
+++ Django-Middleware-0x03/messaging_app/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # Placeholder: chats app urls will be added later
+]
