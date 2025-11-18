356beaa1d742cccc89cc64fc2f353f30947efd60
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-18 11:28:07 +0000

    Update settings.py

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -2,4 +2,5 @@ MIDDLEWARE = [
     "chats.middleware.RequestLoggingMiddleware",
     "chats.middleware.RestrictAccessByTimeMiddleware",
     "chats.middleware.OffensiveLanguageMiddleware",
+    "chats.middleware.RolepermissionMiddleware",
 ]
