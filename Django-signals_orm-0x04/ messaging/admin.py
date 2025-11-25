61ada4245e8f1e42e25cff6b1c1f47d88d460543
Osagyefo Boakye <osagyefokoboakye@gmail.com>
2025-11-25 10:42:58 +0000

    Delete Django-signals_orm-0x04 directory

--- Django-signals_orm-0x04/ messaging/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Message, MessageHistory, Notification
-
-admin.site.register(Message)
-admin.site.register(MessageHistory)
-admin.site.register(Notification)
